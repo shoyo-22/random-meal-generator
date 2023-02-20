8035acc332a8121e91185bf8a2f3aaea0be92d8d
Yessen Murat <shoyo2209@gmail.com>
2023-02-20 23:05:34 +0600

    Update HomeView.vue
    Fetch data from API and display. Just Bluprint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,43 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+let meal = ref({});
+let loading = ref(true);
+
+async function generateRandMeal() {
+  loading.value = true;
+  let response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  let data = await response.json();
+  meal.value = data.meals[0];
+  loading.value = false;
+}
+
+onMounted(() => {
+  generateRandMeal();
+});
+</script>
 
 <template>
-  <main>Main Page</main>
+  <main>
+    <button type="button" @click="generateRandMeal">Generate</button>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <div>ID: {{ meal.idMeal }}</div>
+      <div>Name: {{ meal.strMeal }}</div>
+      <div>Category: {{ meal.strCategory }}</div>
+      <div>Country (origin): {{ meal.strArea }}</div>
+      <div>
+        <h4>Instrcutions</h4>
+        <p>{{ meal.strInstructions }}</p>
+      </div>
+      <div>
+        <img :src="meal.strMealThumb" alt="" />
+      </div>
+      <div>Tags: {{ meal.strTags }}</div>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss"></style>
